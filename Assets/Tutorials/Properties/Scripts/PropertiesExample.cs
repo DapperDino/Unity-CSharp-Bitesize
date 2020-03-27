f917ff0cd849af92b3a12cbf8f45c6b48a9ea52f
Nathan <natemi1010@gmail.com>
2020-03-27 13:50:17 +0000

    Finished Properties video

---
+++ Assets/Tutorials/Properties/Scripts/PropertiesExample.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace DapperDino.Tutorials.Properties
+{
+    public class PropertiesExample : MonoBehaviour
+    {
+        [SerializeField] private float speed = 5f;
+
+        public float Speed => speed;
+
+        private void Test()
+        {
+            speed--;
+        }
+    }
+
+    public class TestingExample
+    {
+        private PropertiesExample propertiesExample;
+
+        private void Test()
+        {
+            float a = propertiesExample.Speed;
+        }
+    }
+}
