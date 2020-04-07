1a43815d058e78d709b9e0a8a5ef03c6055338b6
Nathan <natemi1010@gmail.com>
2020-04-07 10:46:04 +0100

    Finished Coroutines video

---
+++ Assets/Tutorials/Coroutines/Scripts/CoroutineExample.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace DapperDino.Tutorials.Coroutines
+{
+    public class CoroutineExample : MonoBehaviour
+    {
+        [SerializeField] private float duration = 2f;
+
+        private bool isScaling = false;
+
+        private void Update()
+        {
+            if (!Keyboard.current.spaceKey.wasPressedThisFrame || isScaling) { return; }
+
+            StartCoroutine(DoSomething());
+        }
+
+        private IEnumerator DoSomething()
+        {
+            isScaling = true;
+
+            float timer = 0f;
+
+            while (timer < duration)
+            {
+                timer += Time.deltaTime;
+                transform.localScale = Vector3.Lerp(Vector3.one, Vector3.zero, timer / duration);
+                yield return null;
+            }
+
+            yield return new WaitForSeconds(1f);
+
+            timer = 0f;
+
+            while (timer < duration)
+            {
+                timer += Time.deltaTime;
+                transform.localScale = Vector3.Lerp(Vector3.zero, Vector3.one, timer / duration);
+                yield return null;
+            }
+
+            isScaling = false;
+        }
+    }
+}
