9a23655149f2005b03c2c91b897514770ec6e74b
Nathan <natemi1010@gmail.com>
2020-04-10 12:01:29 +0100

    Finished Gizmos video

---
+++ Assets/Tutorials/Gizmos/Scripts/GizmosExample.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace DapperDino.Tutorials.GizmoTools
+{
+    public class GizmosExample : MonoBehaviour
+    {
+        [SerializeField] private float spawnRadius = 5f;
+        [SerializeField] private Transform followTarget = null;
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position, spawnRadius);
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            if (followTarget != null)
+            {
+                Gizmos.color = Color.green;
+                Gizmos.DrawLine(transform.position, followTarget.position);
+            }
+        }
+    }
+}
