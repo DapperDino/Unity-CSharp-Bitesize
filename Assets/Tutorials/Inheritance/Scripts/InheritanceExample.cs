4695d890bba222175d2f09ed60a86a99b4fa339e
Nathan <natemi1010@gmail.com>
2020-04-15 12:40:33 +0100

    Finished Inheritance video

---
+++ Assets/Tutorials/Inheritance/Scripts/InheritanceExample.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace DapperDino.Tutorials.Inheritance
+{
+    public class InheritanceExample : MonoBehaviour
+    {
+        private void Start()
+        {
+            Item item = new Weapon("Sword");
+
+            item.Use();
+        }
+    }
+
+    public class Item
+    {
+        protected readonly string name;
+
+        public Item(string name)
+        {
+            this.name = name;
+        }
+
+        public virtual void Use()
+        {
+            Debug.Log($"Using {name}");
+        }
+    }
+
+    public class Weapon : Item
+    {
+        public Weapon(string name) : base(name) { }
+
+        public override void Use()
+        {
+            Debug.Log($"Swinging {name}");
+        }
+    }
+}
