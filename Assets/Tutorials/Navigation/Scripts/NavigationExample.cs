935d2a856e85a5146cbe1d4049a0b02986ad1ac8
Nathan <natemi1010@gmail.com>
2020-04-21 14:58:20 +0100

    Finished Navigation video

---
+++ Assets/Tutorials/Navigation/Scripts/NavigationExample.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.InputSystem;
+
+namespace DapperDino.Tutorials.Navigation
+{
+    public class NavigationExample : MonoBehaviour
+    {
+        [SerializeField] private NavMeshAgent navMeshAgent = null;
+
+        private Camera mainCamera;
+
+        private void Start() => mainCamera = Camera.main;
+
+        private void Update()
+        {
+            if (!Mouse.current.leftButton.wasPressedThisFrame) { return; }
+
+            FireRaycast();
+        }
+
+        private void FireRaycast()
+        {
+            Ray ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                navMeshAgent.SetDestination(hit.point);
+            }
+        }
+    }
+}
