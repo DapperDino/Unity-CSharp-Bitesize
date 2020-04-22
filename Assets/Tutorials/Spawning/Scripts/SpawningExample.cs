0c505bb3716ee3ec8e6ecfb9968822444d00cdee
Nathan <natemi1010@gmail.com>
2020-04-22 16:12:05 +0100

    Finished Spawning video

---
+++ Assets/Tutorials/Spawning/Scripts/SpawningExample.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace DapperDino.Tutorials.Spawning
+{
+    public class SpawningExample : MonoBehaviour
+    {
+        [SerializeField] private Rigidbody ballPrefab = null;
+
+        private void Update()
+        {
+            if (!Keyboard.current.spaceKey.wasPressedThisFrame) { return; }
+
+            SpawnBall();
+        }
+
+        private void SpawnBall()
+        {
+            Rigidbody ballInstance = Instantiate(ballPrefab, transform.position, transform.rotation);
+
+            ballInstance.AddForce(Vector3.up * 10f, ForceMode.VelocityChange);
+        }
+    }
+}
