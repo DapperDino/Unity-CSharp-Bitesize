e50f75786d5eb668bd22715e9b07a79c87595bf9
Nathan <natemi1010@gmail.com>
2020-04-23 13:57:37 +0100

    Finished Mouse Events video

---
+++ Assets/Tutorials/MouseEvents/Scripts/MouseEventsExample.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+namespace DapperDino.Tutorials.MouseEvents
+{
+    public class MouseEventsExample : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IDragHandler, IPointerUpHandler
+    {
+        [SerializeField] private Image image = null;
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            image.color = new Color(1, 0, 0, 1);
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            image.color = new Color(1, 1, 1, 1);
+        }
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            transform.position = Mouse.current.position.ReadValue();
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            Debug.Log("Up");
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            Debug.Log("Down");
+        }
+    }
+}
